efec5c955a1e731848fd7ddf9a275ba6456bd3b6
Alexey N <novitskiy.alexey@uasoftservice.com>
2016-04-19 21:27:17 +0300

    Repro implementation for the string property issue

--- SiagodbTest/SiagodbTest/App.cs
+++ SiagodbTest/SiagodbTest/App.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using SiagodbTest.DbModels;
 using Xamarin.Forms;
 
 namespace SiagodbTest
@@ -13,15 +13,32 @@ namespace SiagodbTest
 		{
 		    var btn = new Button() {Text = "Create Siagodb instance"};
 		    btn.Clicked += OnButtonClicked;
+
+            var btnStingTest = new Button() { Text = "Test string field" };
+            btnStingTest.Clicked += OnButtonStingTestClicked;
+
 			// The root page of your application
 			MainPage = new ContentPage {
 				Content = new StackLayout {
 					VerticalOptions = LayoutOptions.Center,
                     HorizontalOptions = LayoutOptions.Center,
-					Children = {btn}
+                    Children = { btn, btnStingTest }
 				}
 			};
 		}
+
+	    private void OnButtonStingTestClicked(object sender, EventArgs e)
+	    {
+            var str = "Very long string 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 ";
+            var database = SiaqodbFactory.GetInstance();
+            database.DropType<SimpleModel>();
+            database.StoreObject(new SimpleModel() { StringField = str }/*, this.transaction*/);
+            MainPage.DisplayAlert("", "We saved an object with property StringField = "+str, "OK").ContinueWith((task) =>
+	        {
+                var model = database.Cast<SimpleModel>().ToList().First();
+                Device.BeginInvokeOnMainThread(() => MainPage.DisplayAlert("", "We got this object from the db and property StringField = " + model.StringField, "OK"));
+	        });
+	    }
 
 	    private void OnButtonClicked(object sender, EventArgs e)
 	    {
---
+++ SiagodbTest/SiagodbTest/DbModels/SimpleModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiagodbTest.DbModels
+{
+    public class SimpleModel
+    {
+        public string StringField { get; set; }
+    }
+}
